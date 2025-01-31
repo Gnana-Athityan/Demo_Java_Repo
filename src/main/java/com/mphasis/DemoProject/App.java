9f72c4c84d0871a2c651aa5a61cb6f184ac69af7
Gnana Athityan <gnana.bs@mphasis.com>
2025-01-31 11:46:49 +0530

    First Commit

---
+++ src/main/java/com/mphasis/DemoProject/App.java
@@ -0,0 +1,19 @@
+package com.mphasis.DemoProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void addData(String Data) {
+		System.out.println("Data "+Data);
+		if (Data.equals("Mphasis")){
+			System.out.println("Welcome");
+		}
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
